2dcdda6b61af76191b0426fe9e96dae2b431c9a2
Mmondal <129409101+M2Mondal@users.noreply.github.com>
2023-03-31 00:00:08 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Addition{
+    
+    public static int sum(int num1, int num2){
+        int addition = num1 + num2;
+        return addition;
+    }
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the first number: ");
+        int n1 = sc.nextInt();
+        
+        System.out.println("Enter the second number: ");
+        int n2 = sc.nextInt();
+        
+        int sum = sum(n1,n2);
+        
+        System.out.println("The sum of "+n1+" and "+n2+" is "+sum);
+    }
+}
