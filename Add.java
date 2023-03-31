294912e7bf4d972cdf506e730ece261be8b70d6b
Monisha Mondal <129409101+M2Mondal@users.noreply.github.com>
2023-04-01 00:05:37 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -7,13 +7,13 @@ class Add {
     static int sumOfDigitsFrom1ToN(int n)
     {
         int result = 0; 
-        for (int x = 1; x <= n; x++)
-            result += sumOfDigits(x);
+        for (int x= 1; x <= n; x++)
+            result =result + Digits(x);
       
         return result;
     }
       
-    static int sumOfDigits(int x)
+    static int Digits(int x)
     {
         int sum = 0;
         while (x != 0)
@@ -29,8 +29,6 @@ class Add {
         System.out.println("Enter the no:");
         Scanner sc= new Scanner(System.in);
         int n=sc.nextInt();
-        System.out.println("Sum of digits in numbers"
-                          +" from 1 to " + n + " is "
-                          + sumOfDigitsFrom1ToN(n));
+        System.out.println("Sum of digits in numbers" + " from 1 to " + n + " is "+ sumOfDigitsFrom1ToN(n));
     }
 }
